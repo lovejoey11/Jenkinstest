5ff63edc7bcddc000c658eda4502052d99dce0a1
lovejoey11 <lyk119@gmail.com>
2017-05-26 19:30:10 -0400

    another try

--- src/com/demo/service/ConnectionProvider.java
+++ src/com/demo/service/ConnectionProvider.java
@@ -32,12 +32,12 @@ public class ConnectionProvider{
 	private static void getCredentials() {
 		try {
 			Properties prop = new Properties();
-			String filename="config.properties";
-			input = ConnectionProvider.class.getClassLoader().getResourceAsStream(filename);
-    		if(input==null){
-    	            System.out.println("Sorry, unable to find " + filename);
-    		    return;
-    		}
+//			String filename="config.properties";
+//			input = ConnectionProvider.class.getClassLoader().getResourceAsStream(filename);
+//    		if(input==null){
+//    	            System.out.println("Sorry, unable to find " + filename);
+//    		    return;
+//    		}
     		//load a properties file from class path, inside static method
     		
     		prop.load(new FileInputStream( getPath()));
