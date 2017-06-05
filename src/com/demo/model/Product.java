6e51ae821e1317007019f4bdc433fbb86adbc7e6
lovejoey11 <lyk119@gmail.com>
2017-06-05 14:32:15 -0400

    implemented basic hibernate persistance layer

--- src/com/demo/model/Product.java
+++ src/com/demo/model/Product.java
@@ -1,24 +1,28 @@
 package com.demo.model;
 
 public class Product {
-	private String productId;
+	private int productId;
 	private String productName;
 	private String productDescption;
+	private double productPrice;
 	
 	public Product() {
 	}
 	
-	public Product(String productId, String productName, String productDescption) {
-		this.productId = productId;
+	public Product(String productName,
+			String productDescption, double productPrice) {
+		super();
+		
 		this.productName = productName;
 		this.productDescption = productDescption;
+		this.productPrice = productPrice;
 	}
 
-	public String getProductId() {
+	public int getProductId() {
 		return productId;
 	}
 
-	public void setProductId(String productId) {
+	public void setProductId(int productId) {
 		this.productId = productId;
 	}
 
@@ -38,8 +42,11 @@ public class Product {
 		this.productDescption = productDescption;
 	}
 
+	public double getProductPrice() {
+		return productPrice;
+	}
 
-	
-	
-	
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
 }
